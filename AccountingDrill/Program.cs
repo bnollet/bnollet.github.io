c96f31fd9340653e7cab4881e2e9d0323d160d22
bnollet <bnollet7@gmail.com>
2018-12-23 10:53:49 -0700

    Accounting Drill
    accounting drill

---
+++ AccountingDrill/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace AccountingDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember we're accounting on you!");
+            Console.ReadLine();
+        }
+    }
+}

6c6b0b64eedde3cacd8df46941d287cb86ee2a6d
bnollet <bnollet7@gmail.com>
2019-01-06 11:19:42 -0700

    Iteration Drill
    whole folder iteration drill

---
+++ Iteration/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+    class Program
+    {
+    static void Main(string[] args)
+    {
+        
+
+        //1)
+        //Console.WriteLine("Please enter your name and press enter");
+        //string userInput = Console.ReadLine();
+        //string[] names = { "Brad", "Greg", "David", "Jamal", "Bill", userInput };
+
+        //foreach (string name in names)
+        //{
+        //    Console.WriteLine(name);
+        //}
+        //Console.ReadLine();
+
+        //2)
+        //bool addition = true;
+
+        //while (addition = true)
+        //{
+        //    Console.WriteLine("two plus two equals 4");
+        //        Console.ReadLine();
+        //}
+
+        //3
+        //bool addition = true;
+
+        //while (addition = true)
+        //{
+        //    Console.WriteLine("two plus two equals 4");
+        //    break;
+        //}
+        //Console.ReadLine();
+
+        //4
+        //List<int> testScores = new List<int>() { 98, 99, 12, 70, 86, 100, 50, 46, };
+
+        //foreach (int score in testScores)
+        //{
+        //    if (score < 84)
+        //    {
+        //        Console.WriteLine("Passing Scores" + score);
+        //    }
+        //}
+        //Console.ReadLine();
+
+        //5
+        //List<int> testScores = new List<int>() { 98, 99, 12, 70, 84, 100, 50, 46, };
+
+        //foreach (int score in testScores)
+        //{
+        //    if (score <= 84)
+        //    {
+        //        Console.WriteLine("Passing Scores" + score);
+        //    }
+        //}
+        //Console.ReadLine();
+
+        //6, 7, 8
+        //List<string> names = new List<string>() { "Bill", "George", "Ben", "Jamal", "Jake", "Fred" };
+        //Console.WriteLine("pick a letter and type it in below ");
+        //string input = Console.ReadLine();
+
+
+        //for (int i = 0; i < names.Count; i++)
+        //{
+        //    if (names[i].Contains(input))
+        //    {
+        //        Console.WriteLine("These strings contained your input: " + i);
+        //        Console.ReadLine();
+        //        break;
+        //    }
+
+
+        //    else
+        //    {
+        //        Console.WriteLine("That letter is not in the List, please guess again:");
+        //        Console.ReadLine();
+        //    }
+
+        //9 10
+        //List<string> names = new List<string>();
+        //names.Add("Brian");
+        //names.Add("Jeff");
+        //names.Add("Jeff");
+        //names.Add("Hank");
+        //names.Add("Walker");
+
+        //Console.WriteLine("Select text to search for in the list");
+        //string input = Console.ReadLine();
+
+        //for (int i = 1; i < 5; i++)
+        //{
+        //    if (names[i].Contains(input)) ;
+        //    Console.WriteLine("Your input matches: " + (names[i].Contains(input)));
+        //}
+        //{
+        //    else
+        //    Console.WriteLine("Your input was not in the list");
+        //}
+        //Console.ReadLine();
+
+        //11
+
+        //List<string> names = new List<string>() { "Bill", "George", "Ben", "Jamal", "Jake", "Fred", "Bill" };
+
+
+        //var dict = new Dictionary<string, int>();
+        //foreach (var count in names)
+        //{
+        //    if (dict.ContainsKey(count))
+        //        dict[count]++;
+        //    else
+        //        dict[count] = 1;
+        //}
+        //foreach (var val in dict)
+        //    Console.WriteLine("{0} occurred {1} times", val.Key, val.Value);
+        //Console.ReadLine();
+
+    }
+}
+
